851fa5ef7d905864273d88938fc327a95f552600
Erick Rick <erickcredjes@gmail.com>
2022-05-29 19:55:00 -0300

    Cronometro simples

---
+++ StopWatch/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StopWatch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Menu.Start();
+        }
+
+    }
+}

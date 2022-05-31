9fa8c65f16dd9de49e4e87af25487e27bb16b18f
Erick Rick <erickcredjes@gmail.com>
2022-05-31 19:02:04 -0300

    Iniciando com Exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exceptions
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // ExemploDeErro.Start();
+
+            // DisparandoExceptions.Start();
+
+            // ExceptionsCostumizada.Start();
+
+            IniciandoFinally.Start();
+        }
+    }
+}

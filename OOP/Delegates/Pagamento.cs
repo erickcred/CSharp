c8bc5babeca7b1970a3dd4f5398c1cff6a54706d
Erick Rick <erickcredjes@gmail.com>
2022-06-04 09:58:34 -0300

    Orientação Objetos

---
+++ OOP/Delegates/Pagamento.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Delegates
+{
+    public class Pagamento
+    {
+        public delegate void Pagar(double valor);
+    }
+}
---
+++ OOP/Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Delegates
+{
+    public class Program
+    {
+        // Delegates => Anonymous Methods
+
+        static void RealizarPagamento(double valor)
+        {
+            Console.Clear();
+            // Toda a validação aqui 
+            Console.WriteLine($"Pagamento feito no valor de {valor.ToString("C")}");
+        }
+
+        public static void Main(string[] args)
+        {
+            var pag = new Pagamento.Pagar(RealizarPagamento);
+            pag(30);
+            
+        }
+    }
+}
